c3932b4a03231e0ba78ab5c696c047fbd66b2ed7
Jovana Stakić <jovana.stakic997@gmail.com>
2022-12-05 16:55:52 +0100

    Napravljeni tabovi, loginPage i pocetne rute app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import{ redirectLoggedInTo, redirectUnauthorizedTo, canActivate} from '@angular/fire/auth-guard';
+const redirectUnauthorizedToLogIn=()=>redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome=()=>redirectLoggedInTo(['tabs']);
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    ...canActivate(redirectUnauthorizedToLogIn)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome)
   }
 ];
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'my-recepie-87287',
+    appId: '1:1031999562915:web:9377f3aa6b73bb0a3c0b16',
+    storageBucket: 'my-recepie-87287.appspot.com',
+    apiKey: 'AIzaSyC862vlwCYy5Vg6B72zSnpegO_DAgfGG8k',
+    authDomain: 'my-recepie-87287.firebaseapp.com',
+    messagingSenderId: '1031999562915',
+  },
   production: true
 };
